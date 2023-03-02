41206a454a115baf1204ddd570a796272e0a9b56
Porta <portalmaster137@gmail.com>
2023-03-02 21:02:17 +1000

    mmmmm

---
+++ utility/CreateDotEnv.ts
@@ -0,0 +1,16 @@
+import fs from 'fs';
+
+function CreateDotEnv() {
+    let file = `
+    TOKEN=
+    PREFIX=
+    VERBOSE=false
+    DATABASE_URL="file:./dev.db"
+    `;
+
+    fs.writeFile('.env', file, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
+}
+
